bd6c4796effbe84df15cd030c4ad4a37b38d033b
RuslanaManbatchurina <ruslanany66@gmail.com>
2023-03-19 17:44:43 +0500

    review tests

---
+++ src/main/java/common/Constants.java
@@ -0,0 +1,9 @@
+package common;
+
+public class Constants {
+    public final static float BUN_PRICE = 12.5f;
+    public final static float SAUCE_PRICE = 5.5f;
+    public final static float FILLING_PRICE = 7.5f;
+    public final static float BUN_AND_SAUCE_PRICE = BUN_PRICE * 2 + SAUCE_PRICE;
+    public final static float BUN_AND_FILLING_PRICE = BUN_PRICE * 2 + FILLING_PRICE;
+}
